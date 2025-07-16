8f284589a06056b99dd3083cf606eff0467b152f
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:34 -0400

    Add ConsultaPublica/Infra/Data/Repositories/Interfaces/IRepository.cs 1752677497081

---
+++ ConsultaPublica/Infra/Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Infra.Data.Repositories.Interfaces
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(object id);
+        Task InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(object id);
+        Task SaveAsync();
+    }
+}
