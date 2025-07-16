9bf1796285d4b76909a5fcbbacec32c7390d3e78
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:09 -0400

    Add ConsultaPublica/Domain/Enums/LegislacaoEnum.cs 1752677531742

---
+++ ConsultaPublica/Domain/Enums/LegislacaoEnum.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum LegislacaoEnum
+    {
+        [Display(Name = "14.133/2021")] L14133,
+        [Display(Name = "8.666/1993")] L8666,
+    }
+}
