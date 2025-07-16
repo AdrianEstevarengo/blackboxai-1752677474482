c860308b543c946b79bffc58746f5abc7923413d
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:47 -0400

    Add ConsultaPublica/Domain/Enums/UsuarioFuncaoEnum.cs 1752677509948

---
+++ ConsultaPublica/Domain/Enums/UsuarioFuncaoEnum.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum UsuarioFuncaoEnum
+    {
+        [Display(Name = "Administrador")]
+        ADMIN,
+
+        [Display(Name = "Central de apoio")]
+        CA,
+
+        [Display(Name = "Núcleo de processamento")]
+        NP,
+
+        [Display(Name = "Pregoeiro")]
+        PREGOEIRO,
+
+        [Display(Name = "Atendimento")]
+        ATENDIMENTO
+    }
+}
