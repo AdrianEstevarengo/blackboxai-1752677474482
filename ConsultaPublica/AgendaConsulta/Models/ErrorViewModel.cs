018f3df8bb030219070a346f34b0280f26eee8e3
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:54:36 -0400

    Add ConsultaPublica/AgendaConsulta/Models/ErrorViewModel.cs 1752677618393

---
+++ ConsultaPublica/AgendaConsulta/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AgendaConsulta.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
