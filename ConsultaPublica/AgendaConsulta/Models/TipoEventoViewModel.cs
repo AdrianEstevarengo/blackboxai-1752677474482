338ead4df0071cdd9faf5eddf73a8b84a9b4cf63
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:54:30 -0400

    Add ConsultaPublica/AgendaConsulta/Models/TipoEventoViewModel.cs 1752677612889

---
+++ ConsultaPublica/AgendaConsulta/Models/TipoEventoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendaConsulta.Models
+{
+    public class TipoEventoViewModel
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
