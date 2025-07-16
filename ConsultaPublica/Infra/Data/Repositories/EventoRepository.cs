c47786f7219cdb3305f999bd18b459cfe65accc1
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:39 -0400

    Add ConsultaPublica/Infra/Data/Repositories/EventoRepository.cs 1752677502207

---
+++ ConsultaPublica/Infra/Data/Repositories/EventoRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Infra.Data.Context;
+using Infra.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Repositories
+{
+    public class EventoRepository : Repository<Evento>, IEventoRepository
+    {
+        private readonly AionDbContext _aionContext;
+
+        public EventoRepository(AionDbContext context) : base(context)
+        {
+            _aionContext = context;
+        }
+
+        public async Task<IEnumerable<Evento>> ConsultarEventosPorAtividadeId(Guid atividadeId)
+        {
+            var eventos = await _aionContext
+                .Eventos.AsNoTracking()
+                .Where(e => e.AtividadeId == atividadeId)
+                .ToListAsync();
+
+            return eventos;
+        }
+    }
+}
