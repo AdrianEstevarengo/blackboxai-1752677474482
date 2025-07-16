5ab7bc7493f01aa0ee58047ca04c26e1734d782d
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:48 -0400

    Add ConsultaPublica/Domain/Enums/TipoPregoeiro.cs 1752677510968

---
+++ ConsultaPublica/Domain/Enums/TipoPregoeiro.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum TipoPregoeiro
+    {
+        [Display(Name = "Pregoeiro")] Pregoeiro,
+        [Display(Name = "Pregoeiro Substituto")] PregoeiroSubstituto,
+    }
+}
