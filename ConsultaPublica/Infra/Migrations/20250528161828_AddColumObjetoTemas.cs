6602cc01185eddc6224f0cdb5d2517f31f9d6f69
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:22 -0400

    Add ConsultaPublica/Infra/Migrations/20250528161828_AddColumObjetoTemas.cs 1752677485168

---
+++ ConsultaPublica/Infra/Migrations/20250528161828_AddColumObjetoTemas.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumObjetoTemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Objeto",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Temas",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Objeto",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda");
+
+            migrationBuilder.DropColumn(
+                name: "Temas",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda");
+        }
+    }
+}
