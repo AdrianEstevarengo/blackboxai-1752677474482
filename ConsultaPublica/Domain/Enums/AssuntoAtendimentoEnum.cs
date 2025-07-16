e5e7b0a40ba83419ccd980f2e23e48412acca4b7
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:22 -0400

    Add ConsultaPublica/Domain/Enums/AssuntoAtendimentoEnum.cs 1752677544486

---
+++ ConsultaPublica/Domain/Enums/AssuntoAtendimentoEnum.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum AssuntoAtendimentoEnum
+    {
+        [Display(Name = "Chamamento público")]
+        ChamamentoPublico,
+
+        [Display(Name = "Pregão Eletrônico")]
+        PregaoEletronico,
+
+        [Display(Name = "Processo sobre obras")]
+        ProcessoObras,
+
+        [Display(Name = "Planilha de composição de custos")]
+        PlanilhaComposicaoCustos,
+
+        [Display(Name = "Dificuldade de consultas a processos")]
+        DificuldadesConsultaProcessos,
+
+        [Display(Name = "Outros")]
+        Outros
+    }
+}
