587cd52b1ed907a6a6e2feb082ead6e30bb056e9
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:59 -0400

    Add ConsultaPublica/AgendaConsulta/wwwroot/js/site.js 1752677581469

---
+++ ConsultaPublica/AgendaConsulta/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+
+    // Aplica a máscara de entrada no campo de processo
+    $('.mascara-processo').inputmask({
+        mask: "9999.999999/9999-99[[-C9]]"
+    });
+});
