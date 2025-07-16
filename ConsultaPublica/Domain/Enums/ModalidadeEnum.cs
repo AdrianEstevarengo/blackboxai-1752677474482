f87ec434fc5af8472dcb2f5c0625691a6e0ec054
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:05 -0400

    Add ConsultaPublica/Domain/Enums/ModalidadeEnum.cs 1752677527462

---
+++ ConsultaPublica/Domain/Enums/ModalidadeEnum.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum ModalidadeEnum
+    {
+        [Display(Name = "Chamamento Público")]
+        CHP,
+
+        [Display(Name = "Concorrência")]
+        CP,
+
+        [Display(Name = "Pregão Eletrônico")]
+        PE,
+
+        [Display(Name = "Tomada de Preços")]
+        TP,
+
+        [Display(Name = "Pregão Eletrônico para Registro de Preços")]
+        PERP,
+
+        [Display(Name = "Pregão Presencial para Registro de Preços")] //REMOVER DPS ESPERANDO PAMELA
+        PPRP,
+    }
+}
