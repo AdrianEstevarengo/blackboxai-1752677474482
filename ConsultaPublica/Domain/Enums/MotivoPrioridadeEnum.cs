127b385641cec1cd77f267cbe0df777b5682f077
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:03 -0400

    Add ConsultaPublica/Domain/Enums/MotivoPrioridadeEnum.cs 1752677526342

---
+++ ConsultaPublica/Domain/Enums/MotivoPrioridadeEnum.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum MotivoPrioridadeEnum
+    {
+        [Display(Name = "Evento")] Evento,
+        [Display(Name = "Pedido da unidade")] PedidoUnidade
+    }
+}
