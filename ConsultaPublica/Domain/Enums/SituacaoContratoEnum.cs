dbbd0ab7f56e462168d21c3e6fe2e532a0559a6a
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:55 -0400

    Add ConsultaPublica/Domain/Enums/SituacaoContratoEnum.cs 1752677517570

---
+++ ConsultaPublica/Domain/Enums/SituacaoContratoEnum.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum SituacaoContratoEnum
+    {
+        [Display(Name = "Pagamento")]
+        Pagamento,
+        [Display(Name = "Liquidação")]
+        Liquidacao,
+        [Display(Name = "Concluído")]
+        Concluido,
+        [Display(Name = "Pendência")]
+        Pendencia
+
+    }
+}
