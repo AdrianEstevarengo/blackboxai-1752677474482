86cfca1549d15b50ece3c014b1385c8fa720fbfb
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:46 -0400

    Add ConsultaPublica/Infra/CrossCutting/Helpers/EnumHelper.cs 1752677508866

---
+++ ConsultaPublica/Infra/CrossCutting/Helpers/EnumHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infra.CrossCutting.Helpers
+{
+    public static class EnumExtensions
+    {
+        private static readonly ConcurrentDictionary<Enum, string> DisplayNameCache = new();
+
+        public static string ToStringRepresentation<T>(this T enumValue)
+            where T : Enum
+        {
+            return enumValue.ToString();
+        }
+
+        public static T ToEnumValue<T>(this string stringValue)
+            where T : Enum
+        {
+            if (Enum.TryParse(typeof(T), stringValue, true, out var result))
+            {
+                return (T)result;
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"'{stringValue}' is not a valid value for enum {typeof(T).Name}."
+                );
+            }
+        }
+
+        public static string GetDisplayName<T>(this T? enumValue)
+            where T : struct, Enum
+        {
+            return enumValue.HasValue
+                ? GetDisplayName(enumValue.Value)
+                : throw new ArgumentNullException(nameof(enumValue), "Enum value cannot be null.");
+        }
+
+        public static string GetDisplayName<T>(this T enumValue)
+            where T : Enum
+        {
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+            if (fieldInfo == null)
+                return enumValue.ToString();
+
+            var displayAttribute =
+                fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), false).FirstOrDefault()
+                as DisplayAttribute;
+
+            return displayAttribute?.Name ?? enumValue.ToString();
+        }
+    }
+}
