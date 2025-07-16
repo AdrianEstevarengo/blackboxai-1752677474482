01b821c81564064230791af3852aec11c9f0ad2b
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:54:33 -0400

    Add ConsultaPublica/AgendaConsulta/Models/HomeViewModel.cs 1752677616183

---
+++ ConsultaPublica/AgendaConsulta/Models/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AgendaConsulta.ViewModels;
+using Domain.Entities;
+
+namespace AgendaConsulta.Models
+{
+    public class HomeViewModel
+    {
+        public List<ProcessoAgendaViewModel> Processos { get; set; }
+        public List<AtividadeViewModel> Atividades { get; set; }
+    }
+}
