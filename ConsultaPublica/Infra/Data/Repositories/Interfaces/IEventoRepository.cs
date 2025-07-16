675b438b8752fdad5ae779667f5168968e58480a
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:36 -0400

    Add ConsultaPublica/Infra/Data/Repositories/Interfaces/IEventoRepository.cs 1752677498569

---
+++ ConsultaPublica/Infra/Data/Repositories/Interfaces/IEventoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Infra.Data.Repositories.Interfaces
+{
+    public interface IEventoRepository : IRepository<Evento>
+    {
+        Task<IEnumerable<Evento>> ConsultarEventosPorAtividadeId(Guid atividadeId);
+    }
+}
