46076d9c69634b7100a7a91904bf783be4116984
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:07 -0400

    Add ConsultaPublica/Domain/Enums/MesaEnum.cs 1752677529486

---
+++ ConsultaPublica/Domain/Enums/MesaEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Enums
+{
+    public enum MesaEnum
+    {
+        COEDU, // Antiga ÔMEGA
+        COESP, // Antiga CELL
+        COGEN1, // Antiga GAMA
+        COGEN2, // Antiga KAPPA
+        COGEN3, // Antiga ZETA
+        COGEN4, // Antiga BETA
+        COGEN5, // recém instituída SUPEL_COGEN
+        COOBR, // Antigo CPLO
+        COSEG, // Antiga ALFA
+        COSAU1, // Antiga UPSILON
+        COSAU2, // Antiga DELTA
+        COSAU3, // Antiga EPSILON
+        COSAU4, // Antiga SIGMA
+        Todas
+    }
+}
