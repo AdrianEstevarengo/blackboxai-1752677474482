deb90d54f9b5a48a23f39f8953780c3f0eef9ea9
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:38 -0400

    Add ConsultaPublica/Infra/Data/Repositories/Interfaces/IAgendaRepository .cs 1752677501141

---
+++ ConsultaPublica/Infra/Data/Repositories/Interfaces/IAgendaRepository .cs
@@ -0,0 +1,11 @@
+﻿//using Domain.DTOs;
+//using Domain.Entities;
+
+//namespace Infra.Data.Repositories.Interfaces
+//{
+//    public interface IAgendaRepository : IRepository<ProcessoAgenda>
+//    {
+//        Task<IEnumerable<ProcessoAgenda>> BuscarProcessosPorTermo(ConsultaDto consulta, string ug);
+//        Task<IEnumerable<ProcessoAgenda>> GetProcessoByUGAsync(string ug);
+//    }
+//}
