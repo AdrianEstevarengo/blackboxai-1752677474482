849913de810467c928c4098b54faceac18973617
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:43 -0400

    Add ConsultaPublica/Infra/Data/Context/AgendaDbContext.cs 1752677505412

---
+++ ConsultaPublica/Infra/Data/Context/AgendaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Context;
+
+public class AgendaDbContext : DbContext
+{
+    public AgendaDbContext(DbContextOptions<AgendaDbContext> options)
+        : base(options) { }
+
+    public DbSet<Atendente> Atendentes { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.HasDefaultSchema("AgendaConsulta");
+    }
+}
