c36d726f47f9b1020a1465c04c2055a92c4046b1
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:17 -0400

    Add ConsultaPublica/Domain/Enums/FuncionarioCargoEnum.cs 1752677539570

---
+++ ConsultaPublica/Domain/Enums/FuncionarioCargoEnum.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum FuncionarioCargoEnum
+    {
+        [Display(Name = "Assesor(a)")]
+        Assesor,
+        [Display(Name = "Chefe e Assesoria Tecnica")]
+        ChefeEAssesoriaTecncica,
+        [Display(Name = "Chefe De Unidade")]
+        ChefeUnicade,
+        [Display(Name = "Chefe Gabinete")]
+        ChefeGabinete,
+        [Display(Name = "Coordenador(a)")]
+        Coordenador,
+        [Display(Name = "Controlador(a) Interno(a)")]
+        ControladorInterno,
+        [Display(Name = "Diretor(a) Executivo(a)")]
+        DiretorExecutivo,
+        [Display(Name = "Pregoeiro(a)")]
+        Pregoeiro,
+    }
+}
