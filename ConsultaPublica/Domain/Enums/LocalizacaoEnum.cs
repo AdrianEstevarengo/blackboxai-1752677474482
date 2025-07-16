d8cbca3b2cfbb6876df8e163659d6fb1c1d6d1b2
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:08 -0400

    Add ConsultaPublica/Domain/Enums/LocalizacaoEnum.cs 1752677530664

---
+++ ConsultaPublica/Domain/Enums/LocalizacaoEnum.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum LocalizacaoEnum
+    {
+        [Display(Name = "Interno")]
+        Interno,
+
+        [Display(Name = "Externo")]
+        Externo,
+
+        [Display(Name = "Finalizado")]
+        Finalizado,
+    }
+}
