d6232188e81de822bb7fac87c9e09d2c2d6f6a99
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:13 -0400

    Add ConsultaPublica/Domain/Enums/FuncionarioClassificacao.cs 1752677535721

---
+++ ConsultaPublica/Domain/Enums/FuncionarioClassificacao.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum FuncionarioClassificacaoEnum
+    {
+        [Display(Name = "Pregoeiro")]
+        Pregoeiro,
+        [Display(Name = "Pregoeiro Substituto")]
+        PregoeiroSubstituto,
+        [Display(Name = "Fiscal Contrato")]
+        FiscalContrato,
+        [Display(Name = "Responsavel Contrato")]
+        ResponsavelContrato,
+    }
+}
