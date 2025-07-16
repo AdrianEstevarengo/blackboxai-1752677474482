592dc7cf31af905dbba7459fa5ebbabdbddebd61
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:44 -0400

    Add ConsultaPublica/Domain/DTOs/ItemCollectionDto.cs 1752677567349

---
+++ ConsultaPublica/Domain/DTOs/ItemCollectionDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.DTOs
+{
+    public class ItemCollectionDto
+    {
+        public Guid Id { get; set; }
+        public string? Quantidade { get; set; }
+        public string? ValorEstimado { get; set; }
+        public string? ValorNegociado { get; set; }
+        public string? Situacao { get; set; }
+        public bool? PossuiCota { get; set; }
+        public string? EmpresaVencedora { get; set; }
+        public bool? MeEpp { get; set; }
+        public bool? DesempateRegional { get; set; }
+        public bool? ro { get; set; }
+    }
+
+}
