cbe13645e2709d5089ea38f61e640f13dfdb9995
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:33 -0400

    Add ConsultaPublica/Infra/Data/Repositories/Interfaces/IUnidadeGestoraRepository.cs 1752677495948

---
+++ ConsultaPublica/Infra/Data/Repositories/Interfaces/IUnidadeGestoraRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Infra.Data.Repositories.Interfaces
+{
+    public interface IUnidadeGestoraRepository : IRepository<UnidadeGestora>
+    {
+        Task<UnidadeGestora?> GetBySiglaAsync(string sigla);
+        Task<IEnumerable<UnidadeGestora>> GetAllUnidadeGestorasAsync();
+    }
+}
