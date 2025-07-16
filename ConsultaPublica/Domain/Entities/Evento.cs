643641fd06d4244f723fd0d136bacdc76f3a998a
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:30 -0400

    Add ConsultaPublica/Domain/Entities/Evento.cs 1752677552819

---
+++ ConsultaPublica/Domain/Entities/Evento.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class Evento
+    {
+        public Guid Id { get; set; }
+        public int TipoEventoId { get; set; }
+        public virtual TipoEvento? TipoEvento { get; set; }
+        public string? Localizacao { get; set; }
+        public string? Setorial { get; set; }
+        public string? UnidadeExterna { get; set; }
+        public DateTime DataInicial { get; set; }
+        public DateTime DataFinal { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public Guid AtividadeId { get; set; }
+        public Atividade? Atividade { get; set; }
+    }
+}
