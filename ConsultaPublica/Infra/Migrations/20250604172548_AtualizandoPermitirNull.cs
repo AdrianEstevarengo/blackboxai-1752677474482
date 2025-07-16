4a99918d993fe28c3ae7fb3257dd6201d339669e
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:18 -0400

    Add ConsultaPublica/Infra/Migrations/20250604172548_AtualizandoPermitirNull.cs 1752677480801

---
+++ ConsultaPublica/Infra/Migrations/20250604172548_AtualizandoPermitirNull.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizandoPermitirNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Situacao",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Objeto",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Situacao",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Objeto",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
