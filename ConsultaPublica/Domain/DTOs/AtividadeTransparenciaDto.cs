8a30cc17bdf13d38f569323daf2e06bb12c6abea
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:50 -0400

    Add ConsultaPublica/Domain/DTOs/AtividadeTransparenciaDto.cs 1752677572601

---
+++ ConsultaPublica/Domain/DTOs/AtividadeTransparenciaDto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+
+namespace Domain.DTOs
+{
+    public class AtividadeTransparenciaDto
+    {
+        public string NumeroProcesso { get; set; } = string.Empty;
+        public string NumeroCertame { get; set; } = string.Empty;
+        public string Objeto { get; set; } = string.Empty;
+        public string IdPNCP { get; set; } = string.Empty;
+        public string Legislacao { get; set; } = string.Empty;
+        public string Situacao { get; set; } = string.Empty;
+        public string Modalidade { get; set; } = string.Empty;
+        public DateTime DataEntrada { get; set; }
+        public EventoDto? UltimoEvento { get; set; }
+        public UnidadeGestora? UnidadeGestora { get; set; }
+        public DateTime? DataAberturaCertame { get; set; }
+        public string? SubObjeto { get; set; }
+    }
+}
