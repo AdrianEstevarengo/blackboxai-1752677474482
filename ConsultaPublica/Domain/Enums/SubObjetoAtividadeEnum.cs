97702254877e334af07a0652d66ea858fa04c580
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:50 -0400

    Add ConsultaPublica/Domain/Enums/SubObjetoAtividadeEnum.cs 1752677513149

---
+++ ConsultaPublica/Domain/Enums/SubObjetoAtividadeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum SubObjetoAtividadeEnum
+    {
+        [Display(Name = "Rondônia Rural Show")]
+        Rondônia_Rural_Show,
+
+        [Display(Name = "Merenda Escolar")]
+        Merenda_Escolar,
+
+        [Display(Name = "JOER")]
+        JOER,
+
+        [Display(Name = "JIR")]
+        JIR,
+    }
+}
