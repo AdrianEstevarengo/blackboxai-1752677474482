3f5a8ffb607505992d700540afb658245f1c2491
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:02 -0400

    Add ConsultaPublica/Domain/Enums/MotivoSubstituicao.cs 1752677525324

---
+++ ConsultaPublica/Domain/Enums/MotivoSubstituicao.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum MotivoSubstituicao
+    {
+        [Display(Name = "Férias")]
+        Ferias,
+
+        [Display(Name = "Licença")]
+        Licenca,
+        Recesso,
+        Atestado,
+
+        [Display(Name = "Capacitação")]
+        Capacitacao
+    }
+}
