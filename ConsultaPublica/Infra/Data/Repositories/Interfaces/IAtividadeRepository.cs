ea3d524df895a4467daaf64dac632b1d5fb9942c
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:37 -0400

    Add ConsultaPublica/Infra/Data/Repositories/Interfaces/IAtividadeRepository.cs 1752677500055

---
+++ ConsultaPublica/Infra/Data/Repositories/Interfaces/IAtividadeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.DTOs;
+using Domain.Entities;
+
+namespace Infra.Data.Repositories.Interfaces
+{
+    public interface IAtividadeRepository : IRepository<Atividade>
+    {
+        Task<IEnumerable<Atividade>> BuscarAtividadesPorTermo(ConsultaDto consulta, string ug);
+        Task<IEnumerable<Atividade>> GetAtividadeByUGAsync(string ug);
+
+        Task<Atividade> GetByNumeroProcessoAsync(string numeroProcesso, string ug);
+        Task<List<Atividade>> GetByNumerosProcessosAsync(List<string> numerosProcessos, string ug);
+    }
+}
