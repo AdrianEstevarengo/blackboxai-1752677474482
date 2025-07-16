160024c54af1e91226a9d19ec1b455d1f2af5041
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:45 -0400

    Add ConsultaPublica/Infra/CrossCutting/Helpers/GlobalServices.cs 1752677507739

---
+++ ConsultaPublica/Infra/CrossCutting/Helpers/GlobalServices.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Infra.CrossCutting.Common
+{
+    public static class GlobalServices
+    {
+        public static HttpClient? HttpClient { get; set; }
+        public static IConfiguration? Configuration { get; set; }
+
+        public static void Configure(HttpClient httpClient, IConfiguration configuration)
+        {
+            HttpClient = httpClient;
+            Configuration = configuration;
+        }
+    }
+}
