cc240c43a4b8d2c60d814bf0c13d1acdab8e5bcb
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:52 -0400

    Add ConsultaPublica/Domain/Enums/SituacaoJulgamenEnum.cs 1752677515346

---
+++ ConsultaPublica/Domain/Enums/SituacaoJulgamenEnum.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum SituacaoJulgamento
+    {
+        [Display(Name = "Deferido")] DEFERIDO,
+        [Display(Name = "Indeferido")] INDEFERIDO,
+        [Display(Name = "Em Julgamento")] JULGAMENTO
+    }
+}
