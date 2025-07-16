3d0f9f4e6c01a02e3e62731089508211393b3ae9
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:47 -0400

    Add ConsultaPublica/Domain/DTOs/EventoDto.cs 1752677569452

---
+++ ConsultaPublica/Domain/DTOs/EventoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.DTOs
+{
+    public class EventoDto
+    {
+        public int TipoEventoId { get; set; }
+        public string? TipoEventoTitulo { get; set; }
+        public string? TipoEventoDescricao { get; set; }
+        public DateTime DataInicial { get; set; }
+        public DateTime DataFinal { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
