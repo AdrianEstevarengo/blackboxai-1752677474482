ff8ead6215eb21c18c01a730a8ef2fdfca3930f2
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:58 -0400

    Add ConsultaPublica/Domain/Enums/SetorialEnum.cs 1752677520959

---
+++ ConsultaPublica/Domain/Enums/SetorialEnum.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Enums
+{
+    public enum SetorialEnum
+    {
+        ASTEC,
+        ATP,
+        CA,
+        CAP,
+        COESP,
+        COOBR,
+        CPEAP,
+        CRP,
+        DE,
+        GAB,
+        NA,
+        NP,
+        Pregoeiro,
+        COSAU1,
+        COSAU3,
+    }
+}
