e6ed6c67906ea901cecafca79f83a9485a27b0f0
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:15 -0400

    Add ConsultaPublica/Domain/Enums/FuncionarioCdsEnum.cs 1752677536878

---
+++ ConsultaPublica/Domain/Enums/FuncionarioCdsEnum.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Enums
+{
+    public enum FuncionarioCdsEnum
+    {
+        CDS1,
+        CDS2,
+        CDS3,
+        CDS4,
+        CDS5,
+        CDS6,
+        CDS7,
+        CDS8,
+        CDS9,
+        CDS10,
+        CDS11,
+        CDS12,
+        CDS13,
+        CDS14,
+        CDS15,
+    }
+}
