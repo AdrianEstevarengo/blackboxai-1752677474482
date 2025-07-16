38912124718a000bc6158dda9781bb8b946aa192
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:27 -0400

    Add ConsultaPublica/Domain/Entities/ProcessoAgenda.cs 1752677549484

---
+++ ConsultaPublica/Domain/Entities/ProcessoAgenda.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class ProcessoAgenda
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string NumeroProcesso { get; set; } = string.Empty;
+        public int UnidadeGestoraId { get; set; }
+        public bool Prioridade { get; set; }
+        public string? IdPrioridade { get; set; }
+        public string? MotivoPrioridade { get; set; }
+        public DateTime? DataConclusaoInterna { get; set; }
+        public DateTime? DataConclusaoExterna { get; set; }
+        public DateTime? DataPrevisao { get; set; }
+        public string? Situacao { get; set; }
+        public string? Objeto { get; set; }
+        public SubObjetoAtividadeEnum? Temas { get; set; }
+        public virtual ICollection<Evento>? Eventos { get; set; } = new List<Evento>();
+    }
+}
