f103d04d1d5a641877c3d0e10f11993012019ee6
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:56 -0400

    Add ConsultaPublica/Domain/Enums/SituacaoAtividadeEnum.cs 1752677518583

---
+++ ConsultaPublica/Domain/Enums/SituacaoAtividadeEnum.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums
+{
+    public enum SituacaoAtividadeEnum
+    {
+        [Display(Name = "Andamento")]
+        Andamento,
+
+        [Display(Name = "Deserto")]
+        Deserto,
+
+        [Display(Name = "Êxito")]
+        Exito,
+
+        [Display(Name = "Finalizado parcial")]
+        Finalizado_parcial,
+
+        [Display(Name = "Fracassado")]
+        Fracassado,
+
+        [Display(Name = "Retorno de fase")]
+        Retorno_de_fase,
+
+        [Display(Name = "Revogado")]
+        Revogado,
+
+        [Display(Name = "Suspenso")]
+        Suspenso,
+
+        [Display(Name = "Termo de Encerramento")]
+        Termo_de_Encerramento,
+    }
+}
