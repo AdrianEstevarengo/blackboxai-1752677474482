8d985c62350b41baa5aca021f919a228451e3030
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:28 -0400

    Add ConsultaPublica/Domain/Entities/Pregoeiro.cs 1752677550528

---
+++ ConsultaPublica/Domain/Entities/Pregoeiro.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Pregoeiro
+{
+    public Guid Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Matricula { get; set; }
+    public TipoPregoeiro Tipo { get; set; }
+    public bool Ativo { get; set; } = true;
+}
