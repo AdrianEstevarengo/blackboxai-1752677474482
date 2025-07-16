bea2b7f3fc08a6ddb13d2725ac93724d28d9fba9
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:31 -0400

    Add ConsultaPublica/Infra/Data/Repositories/UnidadeGestoraRepository.cs 1752677493260

---
+++ ConsultaPublica/Infra/Data/Repositories/UnidadeGestoraRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Infra.Data.Context;
+using Infra.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Repositories
+{
+    public class UnidadeGestoraRepository : Repository<UnidadeGestora>, IUnidadeGestoraRepository
+    {
+        private readonly AionDbContext _aionContext;
+
+        public UnidadeGestoraRepository(AionDbContext context) : base(context)
+        {
+            _aionContext = context;
+        }
+
+        public async Task<IEnumerable<UnidadeGestora>> GetAllUnidadeGestorasAsync()
+        {
+            return await _aionContext.UnidadesGestoras.OrderBy(u => u.Sigla).ToListAsync();
+        }
+
+        public async Task<UnidadeGestora?> GetBySiglaAsync(string sigla)
+        {
+            return await _aionContext
+                .UnidadesGestoras.Where(u =>
+                    u.Sigla!.Equals(sigla, StringComparison.CurrentCultureIgnoreCase)
+                )
+                .FirstAsync();
+        }
+    }
+}
