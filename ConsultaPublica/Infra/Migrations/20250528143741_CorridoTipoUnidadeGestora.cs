ea1ee3426a80df6ec1523b234f6676ca734eeb76
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:25 -0400

    Add ConsultaPublica/Infra/Migrations/20250528143741_CorridoTipoUnidadeGestora.cs 1752677487690

---
+++ ConsultaPublica/Infra/Migrations/20250528143741_CorridoTipoUnidadeGestora.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorridoTipoUnidadeGestora : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProcessosAgenda_UnidadesGestoras_UnidadeGestoraId",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProcessosAgenda_UnidadeGestoraId",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Aion");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcessosAgenda_UnidadeGestoraId",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda",
+                column: "UnidadeGestoraId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProcessosAgenda_UnidadesGestoras_UnidadeGestoraId",
+                schema: "AgendaConsulta",
+                table: "ProcessosAgenda",
+                column: "UnidadeGestoraId",
+                principalSchema: "Aion",
+                principalTable: "UnidadesGestoras",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
