9046040b1c2d693fbc3d7ec54a1373e49da47dee
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:45 -0400

    Add ConsultaPublica/Domain/DTOs/FornecedorDTO.cs 1752677568415

---
+++ ConsultaPublica/Domain/DTOs/FornecedorDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.DTOs
+{
+    public class FornecedorDTO
+    {
+        public string niFornecedor { get; set; } = string.Empty;
+        public string nomeRazaoSocialFornecedor { get; set; } = string.Empty;
+        public string? porteFornecedorId { get; set; } //bool
+        public string? porteFornecedorNome { get; set; } //bool
+
+        public bool IsMEPorteFornecedorNomeBool
+        {
+            get { return porteFornecedorNome == "ME"; }
+        }
+        public bool IsMEPortePorteFornecedorIdBool
+        {
+            get { return porteFornecedorId == "1"; }
+        }
+    }
+}
