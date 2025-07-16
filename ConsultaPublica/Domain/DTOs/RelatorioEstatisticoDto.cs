de667dad156ef4cee15a28b1b2d028134b6f891e
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:41 -0400

    Add ConsultaPublica/Domain/DTOs/RelatorioEstatisticoDto.cs 1752677564090

---
+++ ConsultaPublica/Domain/DTOs/RelatorioEstatisticoDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.DTOs
+{
+    public class RelatorioEstatisticoDto
+    {
+        public Guid Id { get; set; }
+        public string? Legislacao { get; set; }
+        public string? NumeroDoProcesso { get; set; }
+        public string? NumeroDoCertame { get; set; }
+        public string? Objeto { get; set; }
+        public string? IdContratacaoPNCP { get; set; }
+        public string? dataPublicacaoPncp { get; set; }
+        public string? dataAberturaProposta { get; set; }
+        public string? dataEncerramentoProposta { get; set; }
+        public DateTime DataCriacaoRelatorio { get; set; }
+        public string? Ano { get; set; }
+        public string? ValorEstimadoGlobal { get; set; }
+        public string? ValorNegociadoGlobal { get; set; }
+        public string? Pregoeiro { get; set; }
+        public string? ElaboradoPor { get; set; }
+        public string? RevisadoPor { get; set; }
+        public List<ItemCollectionDto> ItemsCollection { get; set; } = new();
+    }
+}
