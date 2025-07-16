5eeb5c45e064d7b5b33959748d023c7766b2b787
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:24 -0400

    Add ConsultaPublica/Domain/Entities/TipoEvento.cs 1752677546504

---
+++ ConsultaPublica/Domain/Entities/TipoEvento.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class TipoEvento
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
