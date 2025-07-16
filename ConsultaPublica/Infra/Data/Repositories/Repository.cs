beb007429a176dec091ca2b505a3fbaaf63f7e30
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:32 -0400

    Add ConsultaPublica/Infra/Data/Repositories/Repository.cs 1752677494675

---
+++ ConsultaPublica/Infra/Data/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using Infra.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Repositories
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(object id)
+        {
+            T? entity = await _dbSet.FindAsync(id);
+            return entity!;
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            await SaveAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(object id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id), "O ID não pode ser nulo.");
+            }
+            T? entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await SaveAsync();
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
