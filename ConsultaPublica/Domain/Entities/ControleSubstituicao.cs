e7be2071f477acc732aa55846f5bf750618e8ab0
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:32 -0400

    Add ConsultaPublica/Domain/Entities/ControleSubstituicao.cs 1752677554470

---
+++ ConsultaPublica/Domain/Entities/ControleSubstituicao.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class ControleSubstituicao
+{
+    public Guid Id { get; set; }
+    public Guid PregoeiroId { get; set; }
+    public Pregoeiro? Pregoeiro { get; set; }
+    public Guid PregoeiroSubstitutoId { get; set; }
+    public Pregoeiro? PregoeiroSubstituto { get; set; }
+    public string? PregoeiroNome { get; set; }
+    public string? PregoeiroSubstitutoNome { get; set; }
+    public MotivoSubstituicao Motivo { get; set; }
+    public int Periodo { get; set; }
+    public PagamentoSubstituicao Pagamento { get; set; }
+    public DateTime DataInicial { get; set; }
+    public DateTime DataFinal { get; set; }
+}
