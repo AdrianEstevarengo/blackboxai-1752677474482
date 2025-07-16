9b189610dd5cdf13e22e95324cc48864ccd4a552
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:53:37 -0400

    Add ConsultaPublica/Domain/Entities/Atendimento.cs 1752677559852

---
+++ ConsultaPublica/Domain/Entities/Atendimento.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Atendimento
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Assunto { get; set; } = string.Empty;
+        public string CpfCnpj { get; set; } = string.Empty;
+        public DateTime DataCriacao { get; set; }
+        public string GrauAtendimento { get; set; } = "Urgente";
+        public string Codigo { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+        public string Organizacao { get; set; } = string.Empty;
+        public string Observacoes { get; set; } = string.Empty;
+        public string NomeResponsavel { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
