b0b56eb9a26cfb3f44c88f382aae23678151b812
AdrianEstevarengo <adrian18matos@gmail.com>
2025-07-16 10:52:20 -0400

    Add ConsultaPublica/Infra/Migrations/20250604171801_AtualizandoBanco.cs 1752677482947

---
+++ ConsultaPublica/Infra/Migrations/20250604171801_AtualizandoBanco.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizandoBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Anexo",
+                schema: "AgendaConsulta");
+
+            migrationBuilder.DropTable(
+                name: "Evento",
+                schema: "AgendaConsulta");
+
+            migrationBuilder.DropTable(
+                name: "Recurso",
+                schema: "AgendaConsulta");
+
+            migrationBuilder.DropTable(
+                name: "TipoEvento",
+                schema: "AgendaConsulta");
+
+            migrationBuilder.DropTable(
+                name: "Itemsatacollections",
+                schema: "AgendaConsulta");
+
+            migrationBuilder.DropTable(
+                name: "RelatorioEstatistico",
+                schema: "AgendaConsulta");
+
+            migrationBuilder.DropTable(
+                name: "Atividade",
+                schema: "AgendaConsulta");
+
+            migrationBuilder.DropTable(
+                name: "UnidadeGestora",
+                schema: "AgendaConsulta");
+        }
+    }
+}
